3252c274677cc725f4218d145451e1d3328e3d74
Liamdafish <radbuilder100@gmail.com>
2024-06-12 16:43:51 +0000

    ok

--- p.js
+++ p.js
@@ -1,8 +1,13 @@
 const pass = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // To add more users do username: "password",
-    username: "password",
+    tommy: "tommy999", 
+    liam: "creator420", 
+    tony: "tony698", 
+    joe: "joeisgay", 
+    larry: "larry864", 
+    colin: "colin745",
   },
 };
 
