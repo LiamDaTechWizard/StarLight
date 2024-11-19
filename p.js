0898cd7498af62a5526e14d47f395579c2872b7b
LiamDaTechWizard <liamlongovich@gmail.com>
2024-11-19 08:00:41 -0500

    Update p.js

--- p.js
+++ p.js
@@ -1,14 +1,8 @@
 const pass = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
-   tommy: "tommy999", 
-    liam: "creator420", 
-    tony: "tony698", 
-    joe: "joeisgay", 
-    larry: "larry864", 
-    colin: "colin745",
-    mikey: "mikey696",
-    owen: "owenisskibidi",
+   maxim: "maxim1234",
+    liam: "L!am1219",
   },
 };
 
